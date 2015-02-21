b2368d3d5187d5c369ef4db237ccf17ea683e2ad
Collin Anderson <cmawebsite@gmail.com>
2015-02-21 16:42:12 -0500

    rough, rough stuff

--- trees/views.py
+++ trees/views.py
@@ -1,10 +1,20 @@
 from django.http import JsonResponse
-from django.core.files import File
+from django.core.files.base import ContentFile
 
 from .models import Tree
 
 
 def upload(request, id):
     t = Tree.objects.get(id=id)
-    image = t.image_set.create(image=request.FILES.values()[0] if request.FILES else File(request))
-    return JsonResponse({'status': 'lgtm', 'image_id': image.id})
+    if request.FILES:
+        image_file = request.FILES.values()[0]
+    else:
+        image_file = ContentFile(request.body)
+        image_file.name = request.META['HTTP_FILENAME']
+    image = t.image_set.create(image=image_file)
+    return JsonResponse({
+        'id': image.id,
+        'image': image.image.url,
+        'resource_uri': '/api/v1/image/%s/' % image.id,
+        'type': image.type,
+    })
