1e16867d94cd980fcb824fd21af6dcc4b7cd49f5
Collin Anderson <cmawebsite@gmail.com>
2015-02-21 15:31:33 -0500

    image uploading actually works

--- tree_hugger/urls.py
+++ tree_hugger/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include(api.urls)),
-    url(r'^upload/', views.upload),
+    url(r'^upload/(\d+)/', views.upload),
     url(r'^$', render, {'template_name': 'map.html'}),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- trees/views.py
+++ trees/views.py
@@ -1,9 +1,10 @@
 from django.http import JsonResponse
+from django.core.files import File
 
 from .models import Tree
 
 
 def upload(request, id):
     t = Tree.objects.get(id=id)
-    image = t.images.create(image=request.FILES['image'])
+    image = t.image_set.create(image=request.FILES.values()[0] if request.FILES else File(request))
     return JsonResponse({'status': 'lgtm', 'image_id': image.id})
